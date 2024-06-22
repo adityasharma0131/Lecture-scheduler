f0b25f57792fc30b5c86d42cbd670b19d2a7d788
adityasharma0131 <adityasharma0431@gmail.com>
2024-06-22 22:05:11 +0530

    vercel server side

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lecture-scheduler-9ldw.vercel.app/",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
